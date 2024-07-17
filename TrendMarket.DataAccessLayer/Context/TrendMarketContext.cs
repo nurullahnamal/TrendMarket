4c814f10382111efa42b9ff813f00f1fc435265b
Nurullah <Nurullah@DESKTOP-18KR4VC>
2024-07-17 15:40:03 +0300

    ContextDb

---
+++ TrendMarket.DataAccessLayer/Context/TrendMarketContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrendMarket.EntityLayer.Concrete;
+
+namespace TrendMarket.DataAccessLayer.Context
+{
+    public class TrendMarketContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-18KR4VC\\SQLEXPRESS;initial catalog=TrendMarket;integrated security=true");
+
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature>  Features{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Review> Reviews{ get; set; }
+        public DbSet<Slider>Sliders{ get; set; }
+        public DbSet<SocialMedia> SocialMedias{ get; set; }
+
+
+    }
+}
