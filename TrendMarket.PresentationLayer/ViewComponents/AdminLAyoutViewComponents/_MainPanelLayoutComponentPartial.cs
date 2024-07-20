1da6650ebf93b2b136624745620b4bd6474cf3bf
Nurullah <Nurullah@DESKTOP-18KR4VC>
2024-07-20 17:31:13 +0300

    .

---
+++ TrendMarket.PresentationLayer/ViewComponents/AdminLAyoutViewComponents/_MainPanelLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendMarket.PresentationLayer.ViewComponents.AdminLAyoutViewComponents
+{
+    public class _MainPanelLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrendMarket.PresentationLayer/ViewComponents/AdminLAyoutViewComponents/_ScriptsLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendMarket.PresentationLayer.ViewComponents.AdminLAyoutViewComponents
+{
+	public class _ScriptsLayoutComponentPartial:ViewComponent
+	{
+		public  IViewComponentResult Invoke()
+		{
+
+		return View(); }	
+	
+	}
+}
