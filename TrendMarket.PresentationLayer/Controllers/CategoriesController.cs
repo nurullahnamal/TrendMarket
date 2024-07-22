373b0ef6ccba38bd617169450628933073c79e0c
Nurullah <Nurullah@DESKTOP-18KR4VC>
2024-07-22 18:13:50 +0300

    category list

---
+++ TrendMarket.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrendMarket.BusinessLayer.Abstract;
+
+namespace TrendMarket.PresentationLayer.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ICategoryService _categoryService;
+
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService=categoryService;
+        }
+
+	
+
+		public IActionResult CategoryList()
+		{
+			var values = _categoryService.TGetAll();
+
+			return View(values);
+		}
+	}
+}
---
+++ TrendMarket.PresentationLayer/ViewComponents/AdminLAyoutViewComponents/_CustomTemplateLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendMarket.PresentationLayer.ViewComponents.AdminLAyoutViewComponents
+{
+	public class _CustomTemplateLayoutComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
