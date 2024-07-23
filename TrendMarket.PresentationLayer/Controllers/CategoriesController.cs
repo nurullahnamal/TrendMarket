4b47111730d10b325145e6a7025d8b6b2e1e30bf
Nurullah <Nurullah@DESKTOP-18KR4VC>
2024-07-23 16:26:39 +0300

    CategoryList

--- TrendMarket.PresentationLayer/Controllers/CategoriesController.cs
+++ TrendMarket.PresentationLayer/Controllers/CategoriesController.cs
@@ -3,23 +3,17 @@ using TrendMarket.BusinessLayer.Abstract;
 
 namespace TrendMarket.PresentationLayer.Controllers
 {
-	public class CategoriesController : Controller
-	{
-		private readonly ICategoryService _categoryService;
-
-
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
         public CategoriesController(ICategoryService categoryService)
         {
-            _categoryService=categoryService;
+            _categoryService = categoryService;
         }
-
-	
-
-		public IActionResult CategoryList()
-		{
-			var values = _categoryService.TGetAll();
-
-			return View(values);
-		}
-	}
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetAll();
+            return View(values);
+        }
+    }
 }
--- TrendMarket.PresentationLayer/Program.cs
+++ TrendMarket.PresentationLayer/Program.cs
@@ -1,6 +1,17 @@
+using TrendMarket.BusinessLayer.Abstract;
+using TrendMarket.BusinessLayer.Concrete;
+using TrendMarket.DataAccessLayer.Abstract;
+using TrendMarket.DataAccessLayer.Context;
+using TrendMarket.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TrendMarketContext>();
+
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
